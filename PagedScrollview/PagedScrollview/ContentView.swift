cbb22d9922a1ac332ef89e0734dee9b43428e817
Ritwik Singh <warnite@Ritwiks-MacBook-Pro.local>
2023-11-30 23:41:55 +0530

    first commit

---
+++ PagedScrollview/PagedScrollview/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  PagedScrollview
+//
+//  Created by Ritwik Singh on 30/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    private let sampleImages = [ "1", "2", "3", "4"]
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            LazyHStack() {
+                ForEach(sampleImages, id:\.self) { img in
+                    Image(img)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 350, height: 450)
+                        .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/))
+                        .padding(.horizontal, 20)
+                        .scrollTransition(.animated, axis: .horizontal) { content, phase in
+                            content
+                                .opacity(phase.isIdentity ? 1 : 0)
+                                .scaleEffect(phase.isIdentity ? 1 : 0.7)
+                        }
+                }
+            }
+            .scrollTargetLayout()
+        }
+        .scrollTargetBehavior(.viewAligned)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PagedScrollview/PagedScrollview/PagedScrollviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagedScrollviewApp.swift
+//  PagedScrollview
+//
+//  Created by Ritwik Singh on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PagedScrollviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
